b46318462f062d0a0a680857ac98940ebbf56d3a
cesarfrisa <cesarfrisa@yahoo.es>
2013-11-12 13:36:15 +0100

    p

--- public/index.php
+++ public/index.php
@@ -1,2 +1,48 @@
-<?php
-echo "hola mundo";
+<?php 
+
+
+
+// Define application environment
+defined('APPLICATION_ENV') ||
+	define('APPLICATION_ENV',
+			(getenv('APPLICATION_ENV') ?
+			 getenv('APPLICATION_ENV') : 'production'));
+
+require_once ("../model/generalModel.php");
+require_once ("../model/filesModel.php");
+require_once ("../model/users/file/users.php");
+
+$configFile="../configs/config.ini";
+$config=readConfig($configFile, "production");
+
+
+$request=getRequest();
+
+$layoutparams=array(
+		"request"=>$request,
+		"config"=>$config);
+
+switch ($request['controller'])
+{
+	case 'users':
+		echo renderLayout('login', $request['controller'],$layoutparams);
+	break;
+	
+	case 'index':
+		echo renderLayout('frontend', $request['controller'],$layoutparams);				
+	break;
+	
+	case 'backend':
+		echo renderLayout('backend', $request['controller'],$layoutparams);
+		break;
+	default:
+	break;
+}
+
+
+
+
+
+
+
+
