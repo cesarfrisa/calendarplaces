b9b72feab504ce8727509fec07b64abe9794eefc
cesarfrisa <cesarfrisa@yahoo.es>
2013-11-13 13:29:17 +0100

    intento 2

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -13,7 +13,6 @@ class Bootstrap
 		require_once ("../application/model/generalModel.php");
 		$this->config=readConfig($configFile, APPLICATION_ENV);
 		$this->configApp();
-<<<<<<< HEAD
 	}
 	
 	public function configApp()
@@ -22,29 +21,9 @@ class Bootstrap
 		$this->_session();
 		$this->_layoutparams();
 		//$this->routes();
-		//$this->db();		
+		//$this->db();	
 	}
-	
-	protected function _layoutparams()
-	{
-		$this->layoutparams=array(
-				"request"=>$this->request,
-				"config"=>$this->config);		
-=======
->>>>>>> b6e99c6ad38db4a1c14dfcc807535928b1741326
-	}
-	
-	public function configApp()
-	{
-<<<<<<< HEAD
-=======
-		$this->_request();
-		$this->_session();
-		$this->_layoutparams();
-		//$this->routes();
-		//$this->db();
-	}
-	
+		
 	protected function _layoutparams()
 	{
 		$this->layoutparams=array(
@@ -54,31 +33,20 @@ class Bootstrap
 	
 	public function run()
 	{		
->>>>>>> b6e99c6ad38db4a1c14dfcc807535928b1741326
 		$this->dispatch();
 	}
 	
 	protected function _request()
 	{
 		$this->request=getRequest();
-<<<<<<< HEAD
 	}
-=======
-	}	
->>>>>>> b6e99c6ad38db4a1c14dfcc807535928b1741326
 	
 	protected function _session()
 	{
 		session_start();
 		$this->sessionId=session_id();
-<<<<<<< HEAD
-		return $this->sessionId;
-	}
-=======
 		return $this->sessionId;	
 	} 
->>>>>>> b6e99c6ad38db4a1c14dfcc807535928b1741326
-	
 	public function dispatch()
 	{
 		$controllerName = "Controllers_".ucfirst($this->request['controller']);
